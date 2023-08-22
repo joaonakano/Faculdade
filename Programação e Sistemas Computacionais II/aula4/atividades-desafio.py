72b2eb916999b508095d11997542ed50c9392e92
João Nakano <138939556+joaonakano@users.noreply.github.com>
2023-08-22 08:30:00 -0300

    Atualizacao

--- Programação e Sistemas Computacionais II/aula4/atividades-desafio.py
+++ Programação e Sistemas Computacionais II/aula4/atividades-desafio.py
@@ -15,18 +15,23 @@ print(f"União: {uniao}\nIntersecção: {interseccao}\nDiferença: {diferenca}\n
 # Crie um conjunto com as vogais (a, e, i, o, u) e peça ao usuário para digitar uma palavra. Imprima as vogais que aparecem na palavra digitada.
 conjunto3 = {'a', 'e', 'i', 'o', 'u'}
 palavra = input("Digite uma palavra: ")
+verificador = 0
 for i in palavra:
     if i in conjunto3:
-        print(i)
+        verificador += 1
+        print(f"Vogal encontrada! -> {i}")
+if verificador == 0:
+        print("Nao ha vogais!\n")
+print('\n')
 
 # Crie dois conjuntos com nomes de frutas e verifique se há alguma fruta em comum entre os conjuntos.
 conjunto4_1 = {"abacate", "uva", "limao", "melancia", "kiwi", "laranja", "maca"}
 conjunto4_2 = {"caju", "melancia", "laranja"}
 for i in conjunto4_2:
     if i in conjunto4_1:
-        print(f"Existe {i} no conjunto 4_2\n")
-    else:
-        print("Não existe frutas em comum entre os conjuntos\n")
+        print(f"Existe {i} no conjunto 4_2")
+    elif i == len(conjunto4_1):
+        print("\nNão existe frutas em comum entre os conjuntos\n")
 
 # Crie um conjunto com números inteiros aleatórios e imprima o maior e o menor número do conjunto.
 conjunto5 = {1,6,4,8,3,9}
@@ -39,7 +44,7 @@ for i in conjunto5:
 for j in conjunto5:
     if j < menor_numero:
         menor_numero = j
-print(f"O maior número na sequência é: {maior_numero}")
+print(f"\nO maior número na sequência é: {maior_numero}")
 print(f"O menor número na sequência é: {menor_numero}\n")
 
 # Crie um conjunto com as cores do arco-íris (vermelho, laranja, amarelo, verde, azul, anil, violeta) e peça ao usuário para digitar uma cor. Verifique se a cor digitada está no conjunto e imprima uma mensagem correspondente.
@@ -54,7 +59,7 @@ else:
 conjunto7_1 = {"segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domingo"}
 conjunto7_2 = {"sábado", "domingo"}
 
-print(conjunto7_1.intersection(conjunto7_2))
+print(conjunto7_1.intersection(conjunto7_2), "\n")
 
 # Crie um conjunto com os números de 1 a 20 e outro conjunto com os números pares de 1 a 10. Imprima a diferença entre os dois conjuntos.
 conjunto8_1 = set()
@@ -74,6 +79,16 @@ for i in conjunto9:
     contagem += 1
 media = media / contagem
 if (media >= 7):
-    print(f"A média do aluno é {media} e ele está APROVADO!")
+    print(f"\nA média do aluno é {media} e ele está APROVADO!")
 else:
-    print(f"A média do aluno é {media} e ele está REPROVADO!")
+    print(f"\nA média do aluno é {media} e ele está REPROVADO!")
+
+# Crie um conjunto com os números primos de 1 a 20 e verifique se um número digitado pelo usuário está no conjunto.
+conjunto10 = set()
+for i in range(20):
+    conjunto10.add(i+1)
+numero_user = int(input("\nDigite um numero: "))
+if numero_user in conjunto10:
+    print(f"O numero {numero_user} existe no conjunto10.")
+else:
+    print(f"O numero {numero_user} nao existe no conjunto10.")
