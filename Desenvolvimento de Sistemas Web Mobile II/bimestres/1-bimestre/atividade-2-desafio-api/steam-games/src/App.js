538dad41ac22410a504faf3fa7987ca8eab6bc38
Joao Nakano <joaovynak@gmail.com>
2024-08-12 20:27:52 -0300

    functioning api consuming

--- Desenvolvimento de Sistemas Web Mobile II/bimestres/1-bimestre/atividade-2-desafio-api/steam-games/src/App.js
+++ Desenvolvimento de Sistemas Web Mobile II/bimestres/1-bimestre/atividade-2-desafio-api/steam-games/src/App.js
@@ -1,38 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react';
+import Home from './Components/Home'
 
-const App=()=>{
-  const [Frase, setFrase] = useState({
-    Icon: '',
-    Frase: ''
-  });
-
-  useEffect(()=>{
-    axios.get('https://api.chucknorris.io/jokes/random')
-    .then(res=>{
-      let dataChuck = res.data;
-      setFrase({Icon: dataChuck.icon_url, Frase: dataChuck.value});
-    })
-    .catch(err=>{
-      console.log(err);
-    })
-  },[]);
-
-  function HandleClick(){
-    window.location.reload();
-  }
-
-  return(
-    <div className="main-content">
-      <h1>Frases do Mito dos Mitos: Bols-... Chuck Norris!</h1>
-      <img src={Frase.Icon} alt="imagem" onClick={HandleClick} />
-      <p>{Frase.Frase}</p>
-    </div>
-  );
-  
+export default function App(){
+    return(
+        <Home />
+    );
 }
-
-
-
-export default App;
