531e89c55d45509cd20859a96e84ada696d11935
Joao Nakano <joaovynak@gmail.com>
2024-08-28 22:09:16 -0300

    finished logic

--- Desenvolvimento de Sistemas Web Mobile II/bimestres/1-bimestre/aula-5-firebase/bd/src/App.js
+++ Desenvolvimento de Sistemas Web Mobile II/bimestres/1-bimestre/aula-5-firebase/bd/src/App.js
@@ -29,6 +29,9 @@ export default function App(){
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
+  const [usuario, setUsuario] = useState(false);
+  const [detalhesUsuario, setDetalhesUsuario] = useState({});
+
   const [post, setPost] = useState([]);
 
   useEffect(() => {
@@ -48,6 +51,72 @@ export default function App(){
     }
     carregarPost();
   }, []);
+
+  useEffect(() => {
+    async function verificarLogin() {
+      onAuthStateChanged(auth, user => {
+        if (user) {
+          // tem usuario logado
+          setUsuario(true);
+          setDetalhesUsuario({
+            uid: user.uid,
+            email: user.email
+          });
+        } else {
+          // não possui usuario logado
+          setUsuario(false);
+          setDetalhesUsuario({});
+        }
+      })
+    }
+    verificarLogin();
+  }, []);
+
+  async function novoUsuario() {
+    await createUserWithEmailAndPassword(auth, email, senha)
+    .then(() => {
+      alert("Usuário cadastrado com sucesso!");
+      setEmail("");
+      setSenha("");
+    })
+    .catch(err => {
+      if (err.code === 'auth/weak-password') {
+        alert("Senha fraca! Utilize uma senha mais segura.");
+      }
+
+      if (err.code === 'auth/email-already-in-use') {
+        alert("Email já cadastrado! Tente novamente com um e-mail diferente ou acesse sua conta.");
+      }
+    });
+  }
+
+  async function logarUsuario() {
+    await signInWithEmailAndPassword(auth, email, senha)
+    .then(value => {
+      alert("Usuário logado com sucesso!");
+      setUsuario(true);
+      setDetalhesUsuario({
+        uid: value.user.uid,
+        email: value.user.email     // Por que nao usamos user.email apenas?
+      });
+      setEmail("");
+      setSenha("");
+    })
+    .catch(err => {
+      if (err.code === 'auth/invalid-email') {}
+        alert("E-mail invalido! Utilize um e-mail válido");
+
+      if (err.code === 'auth/invalid-password') {
+        alert("Senha incorreta! Insira uma senha válida para este usuário");
+      }
+    });
+  }
+
+  async function fazerLogout() {
+    await signOut(auth);
+    setUsuario(false);
+    setDetalhesUsuario({});
+  }
 
   // (C)reate
   async function adicionarPosts() {
@@ -112,6 +181,42 @@ export default function App(){
     <div>
       <h1>ReactJS + Firebase</h1>
       
+      {usuario && (
+        <div>
+          <strong>Seja bem-vindo(a)</strong>
+          <br/>
+          <span>ID: {detalhesUsuario.uid}</span>
+          <br/>
+          <span>Email: {detalhesUsuario.email}</span>
+          <br/>
+          <button onClick={fazerLogout}>Sair</button>
+        </div>
+      )}
+      
+      {!usuario && (
+        <div>
+          <h2>Usuários</h2>
+          <label>Email:</label>
+          <input
+          type="e-mail"
+          placeholder="Digite um e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+    
+          <label>Senha:</label>
+          <input
+          type="password"
+          placeholder="Digite uma senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)} />
+    
+          <button onClick={novoUsuario}>Cadastrar</button>
+          <button onClick={logarUsuario}>Login</button>
+        </div>
+      )}
+
+      <hr/>
+
       <h2>POSTS</h2>
       <label>ID do Post:</label>
       <input
