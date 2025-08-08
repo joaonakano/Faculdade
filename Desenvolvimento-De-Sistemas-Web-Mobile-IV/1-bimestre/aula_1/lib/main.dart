12bc8fd63f208451cf80e7db06f0202c822607ea
Joao Nakano <joaovynak@gmail.com>
2025-08-08 09:42:33 -0300

    refatoracoes gerais

--- Desenvolvimento-De-Sistemas-Web-Mobile-IV/1-bimestre/aula_1/lib/main.dart
+++ Desenvolvimento-De-Sistemas-Web-Mobile-IV/1-bimestre/aula_1/lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import "package:flutter/material.dart";
 
 void main() {
@@ -33,9 +31,9 @@ class PaginaPrincipal extends StatelessWidget {
         
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height: 200,),
-
             const Text(
               "Bem-vindo ao Meu Projeto Integrador!",
               style: TextStyle(
@@ -44,8 +42,8 @@ class PaginaPrincipal extends StatelessWidget {
               ),
             ),
 
-            const SizedBox(height: 30,),
-            
+            SizedBox(height: 20,),
+
             Container(
               decoration: BoxDecoration(
                 color: Colors.blueAccent.withOpacity(0.1),
@@ -53,12 +51,17 @@ class PaginaPrincipal extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10)
               ),
               padding: EdgeInsets.all(12),
-              child: Text(
-                "Esta é a nossa primeira tela construída com Flutter. Explore!"
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              child: const Text(
+                "Esta é a nossa primeira tela construída com Flutter. Explore!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16
+                ),
               ),
             ),
 
-            const SizedBox(height: 40,),
+            SizedBox(height: 30,),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -76,9 +79,9 @@ class PaginaPrincipal extends StatelessWidget {
                 Icon(Icons.code, color: Colors.grey, size: 35),
               ],
             ),
-            
-            SizedBox(height: 30,),
 
+            SizedBox(height: 20,),
+            
             const CardProximaJornada(
               cardTitle: "Próximos Passos",
               cardMessage: "Na próxima aula, vamos adicionar interatividade."
@@ -104,6 +107,7 @@ class CardProximaJornada extends StatelessWidget {
   build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(15),
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         boxShadow: [
@@ -127,7 +131,10 @@ class CardProximaJornada extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          Text(cardMessage),
+          SizedBox(height: 5,),
+          Text(
+            cardMessage,
+            style: TextStyle(fontSize: 14, color: Colors.black87),),
         ],
       ),
     );
