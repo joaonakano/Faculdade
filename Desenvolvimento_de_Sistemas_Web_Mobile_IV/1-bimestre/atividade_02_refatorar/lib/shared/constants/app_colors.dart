df1fbc067c1549258c5ae1ef35dc100136684252
Joao Nakano <joaovynak@gmail.com>
2025-09-02 20:09:38 -0300

    commit coitadilson II

---
+++ Desenvolvimento_de_Sistemas_Web_Mobile_IV/1-bimestre/atividade_02_refatorar/lib/shared/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.lightBlue;
+  static const Color secondary = Colors.white;
+  static const Color primaryAccent = Colors.blueAccent;
+
+  static const Color textPrimary = Colors.black87;
+  static const Color textSecondary = Colors.black54;
+  
+  static const Color boxShadow = Colors.grey;
+}
---
+++ Desenvolvimento_de_Sistemas_Web_Mobile_IV/1-bimestre/atividade_02_refatorar/lib/shared/constants/app_spacings.dart
@@ -0,0 +1,9 @@
+class AppSpacings {
+  static const smallMargin = 10;
+  static const mediumMargin = 20;
+  static const largeMargin = 40;
+
+  static const smallpadding = 15;
+  static const mediumpadding = 20;
+  static const largepadding = 30;
+}
